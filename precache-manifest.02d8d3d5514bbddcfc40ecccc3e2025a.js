d3ad15d504014aba4c63e5d7ce03becd1ddb5922
Andrzej <aserafin4@wp.pl>
2020-10-13 00:19:42 +0200

    Updates

---
+++ precache-manifest.02d8d3d5514bbddcfc40ecccc3e2025a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a4e2e5c5631ef4816a362b8467c1113",
+    "url": "/superhero/index.html"
+  },
+  {
+    "revision": "5a9f36e84918adea02f4",
+    "url": "/superhero/static/css/main.04c0b3fb.chunk.css"
+  },
+  {
+    "revision": "9b2dfe434db34957513c",
+    "url": "/superhero/static/js/2.d2989841.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/superhero/static/js/2.d2989841.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a9f36e84918adea02f4",
+    "url": "/superhero/static/js/main.58accb74.chunk.js"
+  },
+  {
+    "revision": "66655c1f97653a830a84",
+    "url": "/superhero/static/js/runtime-main.28d9394b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/superhero/static/media/logo.5d5d9eef.svg"
+  }
+]);
